3dacc5e6aeabff0fb29e0a51b821bbc09cc2b8f4
paulohfsantos <paulohfsantos1@gmail.com>
2023-07-10 21:25:22 -0300

    chore: implement storybook

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Forms />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style lang="scss">
+@import './assets/main.scss';
+</style>
---
+++ src/stories/app.stories.ts
@@ -0,0 +1,8 @@
+import App from '../app.vue';
+
+export default {
+  title: 'App',
+  component: App,
+
+  tags: ['autodocs'],
+}
